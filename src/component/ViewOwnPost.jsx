e95111d82fe421da7284b353896391f0150ddabf
Vaibhav Jagtap <vaibhavsocial7@gmail.com>
2025-11-05 12:56:45 +0530

    skelton added

--- src/component/ViewOwnPost.jsx
+++ src/component/ViewOwnPost.jsx
@@ -1,12 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts } from "../redux/action/postAction";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PostCard from "./PostCard";
+
+const SkeletonCard = () => (
+  <div className="border rounded-xl p-4 animate-pulse border-gray-300">
+    <div className="h-40 bg-gray-300 rounded-xl mb-4"></div>
+    <div className="h-6 bg-gray-300 rounded w-3/4 mb-2"></div>
+    <div className="h-4 bg-gray-300 rounded w-5/6 mb-1"></div>
+    <div className="h-4 bg-gray-300 rounded w-2/3"></div>
+  </div>
+);
 
 const ViewOwnPost = () => {
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.post);
   const userId = useSelector((state) => state.user.currentUser);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     dispatch(getAllPosts());
@@ -21,25 +31,31 @@ const ViewOwnPost = () => {
     [posts.data]
   );
 
-    useEffect(() => {
+  useEffect(() => {
     if (postStatuses) {
       dispatch(getAllPosts());
     }
   }, [dispatch, postStatuses]);
 
+  const skeletonCount = ownPosts?.length || 6;
+
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
-        {ownPosts?.map((data, index) => (
-          <PostCard
-            key={index}
-            title={data.title}
-            text={data.information}
-            image={data.image}
-            author="own"
-            status={data.status}
-          />
-        ))}
+        {isLoading
+          ? Array(skeletonCount)
+              .fill()
+              .map((_, index) => <SkeletonCard key={index} />)
+          : ownPosts?.map((data, index) => (
+              <PostCard
+                key={index}
+                title={data.title}
+                text={data.information}
+                image={data.image}
+                author="own"
+                status={data.status}
+              />
+            ))}
       </div>
     </>
   );
--- src/component/ViewPost.jsx
+++ src/component/ViewPost.jsx
@@ -1,12 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import PostCard from "./PostCard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllPosts } from "../redux/action/postAction";
+
+const SkeletonCard = () => (
+  <div className="border rounded-xl p-4 animate-pulse border-gray-300">
+    <div className="h-40 bg-gray-300 rounded-xl mb-4"></div>
+    <div className="h-6 bg-gray-300 rounded w-3/4 mb-2"></div>
+    <div className="h-4 bg-gray-300 rounded w-5/6 mb-1"></div>
+    <div className="h-4 bg-gray-300 rounded w-2/3"></div>
+  </div>
+);
 
 const ViewPost = ({ title, text }) => {
   const dispatch = useDispatch();
   const { posts } = useSelector((state) => state.post);
   const userId = useSelector((state) => state.user.currentUser);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     dispatch(getAllPosts());
@@ -20,21 +30,29 @@ const ViewPost = ({ title, text }) => {
 
   const otherPost = posts.data?.filter(
     (post) => post.createdBy?._id !== userId.id
+    
   );
+
+    const skeletonCount = otherPost?.length || 6;
+
 
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
-        {otherPost?.map((data, index) => (
-          <PostCard
-            key={index}
-            title={data.title}
-            text={data.information}
-            image={data.image}
-            author={data.createdBy?.name}
-            status={data.status}
-          />
-        ))}
+        {isLoading
+          ? Array(skeletonCount)
+              .fill()
+              .map((_, index) => <SkeletonCard key={index} />)
+          : otherPost?.map((data, index) => (
+              <PostCard
+                key={index}
+                title={data.title}
+                text={data.information}
+                image={data.image}
+                author={data.createdBy?.name}
+                status={data.status}
+              />
+            ))}
       </div>
     </>
   );
